4566f7d6316b89889264be5d9f377e6ed8eb147b
Petra Kočí <petra.koci95@gmail.com>
2023-03-15 12:59:57 +0100

    added allure reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        env: {
+            allureReuseAfterSpec: true,
+            allure: true
+        },
+        setupNodeEvents(on, config) {
+            allureWriter(on, config);
+            return config;
+        },
     },
-  },
 });
