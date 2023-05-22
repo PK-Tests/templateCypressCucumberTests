92525ac003b714717e5308f628295049b76317f6
Petra Kočí <52009786+PK-Tests@users.noreply.github.com>
2023-05-22 09:36:54 +0000

    remove stringify

--- format-test-results.js
+++ format-test-results.js
@@ -39,7 +39,7 @@ function parseTestResults() {
             const testName = test.name;
             const status = test.status;
             let icon = '';
-            if (status === 'success') {
+            if (status === 'passed') {
                 icon = ':white_check_mark:';
             }
             else if (status === 'failed') {
@@ -54,7 +54,7 @@ function parseTestResults() {
                 const testName = nestedTest.name;
                 const status = nestedTest.status;
                 let icon = '';
-                if (status === 'success') {
+                if (status === 'passed') {
                     icon = ':white_check_mark:';
                 }
                 else if (status === 'failed') {
@@ -69,12 +69,26 @@ function parseTestResults() {
         });
     });
     // Turns resuls back to JSON and writes them to new file
-    const formattedResults = JSON.stringify(testResults, null, 2);
     const slackPayload = {
         text: `New <https://pk-tests.github.io/templateCypressCucumberTests/|Allure report> was just deployed.\n
         Results:\n
-        ${formattedResults}`
+        ${testResults}`
     };
     fs.writeFileSync('payload-slack-content.json', JSON.stringify(slackPayload));
+    //const url =
+    //  'https://hooks.slack.com/services/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';
+    //request.post(
+    //  {
+    //    url: url,
+    //  body: JSON.stringify(JSON.stringify(slackPayload)),
+    //},
+    //(err: any, response: any, body: any) => {
+    //    if (err) {
+    //      console.log(err);
+    //} else {
+    //  console.log('Message sent to Slack');
+    //}
+    //}
+    //);
 }
 parseTestResults();
