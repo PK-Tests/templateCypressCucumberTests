89d0bd0aa47483fb9f2ed8001ea559d03d98fbb3
Petra Kočí <52009786+PK-Tests@users.noreply.github.com>
2023-05-25 20:00:16 +0200

    Format slack message
    * adding json slack payload reformatter function
    
    * try simple payload format
    
    * stringify payload
    
    * change object to string
    
    * fixing concat misuse
    
    * remove stringify
    
    * moving \n
    
    * better formatting
    
    * fix markdown
    
    * markdown formatting change
    
    * changing normal markdown to slack "mrkdwn"
    
    * change level icon, add new line after level
    
    * removing some line breaks
    
    * adding spacing for different levels
    
    * remove some tests, change payload format
    
    * deleting more tests
    
    * change newline placement
    
    * change icons
    
    * fail some tests
    
    * run pipeline always, remove  spaces from payload
    
    * fixing loops, adding some tests back
    
    * another loop fix
    
    * delete payload file, add it to gitignore
    
    * debugging loops
    
    * adding ifs before loops
    
    * adding indentation
    
    * removing some indentation
    
    * final indenation changes
    
    * removing failing tests

---
+++ format-test-results.js
@@ -0,0 +1,80 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+function parseTestResults() {
+    // Reads and parses original test report
+    const rawData = fs.readFileSync('allure-report/data/suites.json');
+    const data = JSON.parse(rawData.toString());
+    let testResults = '';
+    // Uses only values for each test suite name, test name and test result
+    data.children.forEach((suite) => {
+        // suites
+        let result = '';
+        const suiteName = suite.name;
+        result = `\n:arrow_right: *${suiteName}*:\n`;
+        testResults += result;
+        // tests
+        suite.children.forEach((test) => {
+            const testName = test.name;
+            const status = test.status;
+            let icon = '';
+            if (status === 'passed') {
+                icon = '      :large_green_circle:';
+            }
+            else if (status === 'failed') {
+                icon = '      :red_circle:';
+            }
+            else {
+                icon = '\n            :arrow_lower_right:';
+            }
+            result = `${icon} ${testName}\n`;
+            testResults += result;
+            // nested tests
+            if (test.children) {
+                test.children.forEach((nestedTest) => {
+                    const nestedTestName = nestedTest.name;
+                    const nestedTestStatus = nestedTest.status;
+                    let icon = '';
+                    if (nestedTestStatus === 'passed') {
+                        icon = '                  :large_green_circle:';
+                    }
+                    else if (nestedTestStatus === 'failed') {
+                        icon = '                  :red_circle:';
+                    }
+                    result = `${icon} ${nestedTestName}\n`;
+                    testResults += result;
+                });
+            }
+        });
+    });
+    // Turns resuls back to JSON and writes them to new file
+    const slackPayload = {
+        text: `New <https://pk-tests.github.io/templateCypressCucumberTests/|Allure report> was just deployed.\n
+        ${testResults}\n`
+    };
+    fs.writeFileSync('payload-slack-content.json', JSON.stringify(slackPayload));
+}
+parseTestResults();
---
+++ format-test-results.ts
@@ -0,0 +1,59 @@
+import * as fs from 'fs';
+
+function parseTestResults() {
+    // Reads and parses original test report
+    const rawData = fs.readFileSync('allure-report/data/suites.json');
+    const data = JSON.parse(rawData.toString());
+
+    let testResults: string = '';
+
+    // Uses only values for each test suite name, test name and test result
+    data.children.forEach((suite: any) => {
+        // suites
+        let result: string = '';
+        const suiteName = suite.name;
+        result = `\n:arrow_right: *${suiteName}*:\n`
+        testResults += result;
+
+        // tests
+        suite.children.forEach((test: any) => {
+            const testName = test.name;
+            const status = test.status;
+            let icon = '';
+            if (status === 'passed') {
+                icon = '      :large_green_circle:';
+            } else if (status === 'failed') {
+                icon = '      :red_circle:';
+            } else {
+                icon = '\n            :arrow_lower_right:';
+            }
+            result = `${icon} ${testName}\n`;
+            testResults += result;
+
+            // nested tests
+            if (test.children) {
+                test.children.forEach((nestedTest: any) => {
+                    const nestedTestName = nestedTest.name;
+                    const nestedTestStatus = nestedTest.status;
+                    let icon = '';
+                    if (nestedTestStatus === 'passed') {
+                        icon = '                  :large_green_circle:';
+                    } else if (nestedTestStatus === 'failed') {
+                        icon = '                  :red_circle:';
+                    }
+                    result = `${icon} ${nestedTestName}\n`;
+                    testResults += result;
+                });
+            }
+        });
+    });
+
+    // Turns resuls back to JSON and writes them to new file
+    const slackPayload = {
+        text: `New <https://pk-tests.github.io/templateCypressCucumberTests/|Allure report> was just deployed.\n
+        ${testResults}\n`
+    }
+    fs.writeFileSync('payload-slack-content.json', JSON.stringify(slackPayload));
+}
+
+parseTestResults();
