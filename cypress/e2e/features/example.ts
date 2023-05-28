6a88726fd8b9d9f57598efa2e8f12773c65e4ba1
Petra Kočí <petra.koci95@gmail.com>
2023-05-28 18:18:37 +0200

    fixing files after prettier, change to ts

--- cypress/e2e/features/example.ts
+++ cypress/e2e/features/example.ts
@@ -2,25 +2,25 @@
 import { When, Then, Given } from '@badeball/cypress-cucumber-preprocessor';
 
 Given('user navigates to testing site', () => {
-    cy.visit('/')
+	cy.visit('/');
 });
 
 Given('chooses "type" section', () => {
-    cy.get('li>a').contains('type').click();
+	cy.get('li>a').contains('type').click();
 });
 
 When('user fills out credentaials', () => {
-    cy.get('#email1').type('example.email@nonexistinggmail.com');
-    cy.get('#password1').type('XRSIJH76553*/"/(:');
+	cy.get('#email1').type('example.email@nonexistinggmail.com');
+	cy.get('#password1').type('XRSIJH76553*/"/(:');
 });
 
 When('confirms form', () => {
-    cy.get('button').contains('Submit').click();
+	cy.get('button').contains('Submit').click();
 });
 
 Then('form is submitted', () => {
-    cy.get('button').contains('Submit').parents('.well').find('p').then(($confirmation) => {
-        expect($confirmation).to.have.text('Your form has been submitted!');
-        expect($confirmation).to.have.attr('style', 'color: rgb(32, 181, 32);');
-    })
+	cy.get('button').contains('Submit').parents('.well').find('p').then(($confirmation) => {
+		expect($confirmation).to.have.text('Your form has been submitted!');
+		expect($confirmation).to.have.attr('style', 'color: rgb(32, 181, 32);');
+	});
 });
